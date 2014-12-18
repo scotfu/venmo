4754d3d66c97a3a011bae6fc464553953e8a9128
fu <scotfu@gmail.com>
2014-12-17 20:32:08 -0500

    check point before changing to queryset

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 #!/usr/bin/evn python
 from storage import put, get_all
+from settings import *
 
-class Basic:
+class Base(object):
 
     def __int__(self, name):
         self.name = name
 
     def save(self):
-        Basic.__save(self)
+        Base.__save(self)
 
     def __str__(self):
         return self.name
@@ -23,33 +24,62 @@ class Basic:
     def all(kls):
         return get_all(kls)
 
+    @classmethod
+    def query(kls, field_name, value):
+        if field_name in kls.__dict__:
+            queryset = get_all(kls)
+            result = filter(lambda instance: getattr(instance, field_name) == value, queryset)
+            return result
+        else:
+            raise 'No such field'
 
 
-
-class User(Basic):
+class User(Base):
     def __init__(self, name, balance=0):
         self.name = name
         self.balance = balance
 
+    def get_banlance(self):
+        return CURRENCY + str(self.balance / 100.0)
 
-class CreditCard(Basic):
-    def __init__(self, user, card_number):
+    @staticmethod
+    def get_by_name(user_name):
+        for u in User.all():
+            if u.name == user_name:
+                return u
+        return None
+
+        
+
+
+class CreditCard(Base):
+    def __init__(self, user, number):
         self.user = user
-        self.card_number = card_number
+        self.number = number
 
+    @staticmethod
+    def get_by_number(card_number):
+        for c in CreditCard.all():
+            if c.number == card_number:
+                return c
+        return None
+        
 
     @staticmethod
     def query():
         pass
 
 
-class Feed:
+class Feed(Base):
     def __init__(self, user, message):
         self.user = user
         self.message = message
         
+    @staticmethod
+    def get_by_user(user):
+        return filter(lambda f: f.user == user,Feed.all())
 
-class Transaction:
+class Transaction(Base):
     def __init__(self, actor, target, amount, note):
         self.actor = actor
         self.target = target
@@ -57,9 +87,9 @@ class Transaction:
         self.note = note
 
     def save(self):
-        super(Transaction, self).save(self)
-        self.target.balance += amount
-        f1 = Feed(self.actor, "You paid %s %f for %s "%(self.target, self.amount, self.note))
-        f2 = Feed(self.target, "%s paid you %f for %s "%(self.actor, self.amount, self.note))
+        super(Transaction, self).save()
+        self.target.balance += self.amount
+        f1 = Feed(self.actor,  FEED_MSG['payer']%(self.target, self.amount/100.0, self.note))
+        f2 = Feed(self.target, FEED_MSG['payee']%(self.actor, self.amount/100.0, self.note))
         f1.save()
         f2.save()
